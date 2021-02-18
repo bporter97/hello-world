0ef607817d6b59f7b779b5c12c9612deea7fc39a
Blake porter <49182700+bporter97@users.noreply.github.com>
2021-02-17 18:53:10 -0600

    Clean up

--- Java/Main.java
+++ Java/Main.java
@@ -2,9 +2,11 @@
 The main method can also access other classes along with the variables and methods of those classes
 and of objects instantiated from those classes.*/
 
-class Hello1 { //define the controlling class
+class Main
+{ //define the controlling class
   //define main method
-  public static void main(String[] args){
+  public static void main(String[] args)
+  {
     //display text string
     System.out.println("Hello World");
 
