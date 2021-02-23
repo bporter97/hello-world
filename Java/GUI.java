59d8da9a098c5c9656890be689e2d543a1f5bcec
Blake Porter <49182700+bporter97@users.noreply.github.com>
2021-02-23 06:31:08 -0600

    Basic GUI
    Basic GUI implementation using the javax.swing libraries

---
+++ Java/GUI.java
@@ -0,0 +1,48 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+//create GUI for appliciation
+public class GUI implements ActionListener
+{
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI()
+    {
+        frame = new JFrame();
+
+        JButton button = new JButton("Click Me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
+        
+        
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Weather App");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+}
